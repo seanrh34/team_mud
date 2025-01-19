f5cccdefb4351dbd51ec4b0c64c43b6a7a2f4e7b
lim tzemin <limtzemin003@gmail.com>
2025-01-19 11:12:00 +0800

    cleanup main.py

--- backend/api_backend/main.py
+++ backend/api_backend/main.py
@@ -10,8 +10,6 @@ import logging
 from enum import Enum
 import drowsydetector
 import cv2
-import numpy as np
-from PIL import Image
 import matplotlib.pyplot as plt
 import face_recognition
 from scipy.spatial import distance
@@ -43,39 +41,25 @@ app.add_middleware(
     allow_headers=["*"],  # Allow all headers
 )
 
-
-# Load your model here
-
 class ImageData(BaseModel):
     image: str
 
 def process(image_data: str):
-    # Decode base64 image
+    # Decode base64 image and convert 
     img_data = base64.b64decode(image_data.split(',')[1])
-    #image = cv2.cvtColor(img_data, cv2.COLOR_BGR2RGB)
-    #image = cv2.imread(img_data)
-    # Preprocess image (resize, normalize, etc. based on model)
-    #image = cv2.resize(image, (640, 480))  #Resize the image
     np_arr = np.frombuffer(img_data, np.uint8)
-    
-    # Decode image using OpenCV
     image = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
-    
-    # Preprocess image (resize, normalize, etc. based on model)
     image = cv2.resize(image, (640, 480))  # Resize the image#
-    #image = np.array(image) / 255.0  # Normalize to [0, 1]
-    #image = np.expand_dims(image, axis=0)  # Add batch dimension
     return image
 
 
 @app.post("/predict")
 async def predict(data: ImageData):
     image = process(data.image)
-    print(type(image))
+    #print(type(image))
     try:
-        
         eye_flag, mouth_flag = drowsydetector.process_image(image)
-        print(type(image), eye_flag, mouth_flag)
+        #print(type(image), eye_flag, mouth_flag)
         if eye_flag and mouth_flag:
             return {"status": Status.SLEEPING.value}
         else:
