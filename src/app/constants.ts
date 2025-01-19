d4d9e54d8526335631e042fa987b6c5517789337
Ng Tze Han, Benedict <E1406493@u.nus.edu>
2025-01-19 11:05:20 +0800

    final commit

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -2,7 +2,7 @@
 
 export const SHORT_BREAK = 5;
 export const LONG_BREAK = 10;
-export const WORK = 60 * 1; //one minute
+export const WORK = 60 * 10; //one minute
 
 
 export const SLEEPTHRESHOLD = 5;
